c21ec86509d35297b3a34a48c76478b647afe84d
popomore <sakura9515@gmail.com>
2019-02-16 03:10:37 +0800

    feat: first implement

---
+++ test/wrap.test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const koa = require('koa');
+
+describe('test/wrap.test.js', () => {
+
+  it('b', async function() {
+    app = new koa();
+    app.use(wrap(function(req, res, next) {
+      setTimeout(() => {
+        next();
+      }, 1000);
+    }));
+    app.use(wrap(function(req, res) {
+      res.send('a');
+    }));
+
+    request(app.callback())
+      .get('/')
+      .expect({});
+  });
+
+});
